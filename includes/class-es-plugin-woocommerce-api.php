658a01b511aca0217755b7226be26aebad37a1b0
Thales Matoso <thalesm.matoso@gmail.com>
2021-08-20 14:16:50 -0300

    implements server 2

--- includes/class-es-plugin-woocommerce-api.php
+++ includes/class-es-plugin-woocommerce-api.php
@@ -31,8 +31,9 @@ class Es_Plugin_Woocommerce_API
     protected $volumes = array();
     protected $log_isw = true;
     private $enviosimples_url = "";
-    private $enviosimples_production_url = "https://api.enviosimples.com.br";
-    private $enviosimples_sandbox_url    = "https://sandbox-api.enviosimples.com.br";
+    private $enviosimples_production_url = "https://api2.enviosimples.com.br";
+    private $enviosimples_sandbox_url    = "https://sandbox-api2.enviosimples.com.br";
+    private $esAppKey = "72jyDaLhTegEBrj9UCdJwO3cAGfbqvFK"
 
     public function __construct($key = '', $sandbox = 'no')
     {
@@ -50,8 +51,9 @@ class Es_Plugin_Woocommerce_API
         $headers = [
             'Content-Type: application/json',
             'Accept-Encoding: gzip, deflate',
-            "key:{$this->key}"
+            "es-app-key:{$this->esAppKey}"
         ];
+        
 
         if ($this->log_isw) {
             $this->isw_log_envios('call_curl($type,$url,$parameters)', '$headers', $headers);
